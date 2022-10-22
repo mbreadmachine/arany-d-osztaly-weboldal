2c49ce9f0c8a8d3f6d3bc20959e4bdb8a5366c9c
Marcus1YouTube <75576363+Marcus1YouTube@users.noreply.github.com>
2022-10-22 14:09:35 +0200

    fixed problem, done with new feature, merging to main

--- src/pages/ClassMoney.js
+++ src/pages/ClassMoney.js
@@ -1,32 +1,36 @@
-import { Typography } from "@mui/material";
-import { LoadingButton } from '@mui/lab'
-import { Search } from "@mui/icons-material";
+import {
+  Typography,
+  Accordion,
+  AccordionSummary,
+  AccordionDetails,
+} from "@mui/material";
+import { LoadingButton } from "@mui/lab";
+import { Search, ExpandMore } from "@mui/icons-material";
 import React from "react";
 import { NavBar } from "../components/NavBar";
 import { supabase } from "../supabase";
 
 const ClassMoney = () => {
-  const [ currentSearch, setCurrentSearch ] = React.useState("");
-  const [ isLoadingKids, setIsLoadingKids ] = React.useState(false)
-  const [ searchResults, setSearchResults ] = React.useState({})
+  const [currentSearch, setCurrentSearch] = React.useState("");
+  const [savedSearch, setSavedSearch] = React.useState("");
+  const [isLoadingKids, setIsLoadingKids] = React.useState(false);
+  const [searchResults, setSearchResults] = React.useState([]);
 
-  const handleSearch = () => {
-    if (currentSearch === "") return setIsLoadingKids(false)
+  const handleSearch = async () => {
+    if (currentSearch === "") return setIsLoadingKids(false);
     try {
-      const { data, error } = supabase.from("classmoney").select().textSearch("child_name", currentSearch)
-      if (error) throw error
-      setSearchResults(data)
-      
-      setTimeout(() => {
-        console.log(searchResults, "from local")
-        console.log(data, "from sup")
-      }, 1000)
-      
+      const { data, error } = await supabase
+        .from("classmoney")
+        .select()
+        .textSearch("child_name", currentSearch);
+      if (error) throw error;
+      setSearchResults(data);
     } catch (err) {
-      alert("HIBA: " + err.message)
+      alert("HIBA: " + err.message);
     } finally {
-      setCurrentSearch("")
-      setIsLoadingKids(false)
+      setSavedSearch(currentSearch)
+      setCurrentSearch("");
+      setIsLoadingKids(false);
     }
   };
 
@@ -48,21 +52,42 @@ const ClassMoney = () => {
           variant="outlined"
           onClick={() => {
             setIsLoadingKids(true);
-            handleSearch()
+            handleSearch();
           }}
-          sx={{ backgroundColor: "white", marginBottom: "20px", }}
+          sx={{ backgroundColor: "white", marginBottom: "20px" }}
         >
           Keresés
         </LoadingButton>
         <div className="kidContainer">
-          {currentSearch !== "" ? (
-            currentSearch
-          ) : (
-            <Typography variant="h6" align="center">
-              Nem írtad be a keresett gyerek nevét! Írd be, hogy megláthasd,
-              mennyi pénzt adtál be!
-            </Typography>
-          )}
+          {searchResults.length !== 0
+            ? searchResults?.map((kid) => (
+                <>
+                  <Typography variant="h6" align="center">Keresés: "{savedSearch}"</Typography>
+                  <Accordion>
+                    <AccordionSummary
+                      expandIcon={<ExpandMore />}
+                      aria-controls="panel1a-content"
+                    >
+                      <Typography>{kid.child_name}</Typography>
+                    </AccordionSummary>
+                    <AccordionDetails>
+                      <ul>
+                        <li>Január: {kid.reported_cash.january} Ft</li>
+                        <li>Február: {kid.reported_cash.february} Ft</li>
+                        <li>Március: {kid.reported_cash.march} Ft</li>
+                        <li>Április: {kid.reported_cash.april} Ft</li>
+                        <li>Május: {kid.reported_cash.may} Ft</li>
+                        <li>Június: {kid.reported_cash.june} Ft</li>
+                        <li>Szeptember: {kid.reported_cash.september} Ft</li>
+                        <li>Október: {kid.reported_cash.october} Ft</li>
+                        <li>November: {kid.reported_cash.november} Ft</li>
+                        <li>December: {kid.reported_cash.december} Ft</li>
+                      </ul>
+                    </AccordionDetails>
+                  </Accordion>
+                </>
+              ))
+            : ""}
         </div>
       </div>
     </>
