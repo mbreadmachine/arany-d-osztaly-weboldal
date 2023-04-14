9fb9655bf523c1ae3c02c7c7e72974ff7da70931
SmallWebDev <75576363+Marcus1YouTube@users.noreply.github.com>
2023-04-14 07:43:08 +0200

    Final edit to dialog card

--- src/components/HomeWorkCard.js
+++ src/components/HomeWorkCard.js
@@ -77,7 +77,7 @@ export const HomeWorkCard = (props) => {
               {pics.pictures &&
                 pics.pictures.map((pic) => (
                   <Grid item xs="auto">
-                    <Card sx={{ minWidth: 275, maxWidth: "350px" }}>
+                    <Card sx={{ minWidth: 275, maxWidth: "300px" }}>
                       <div
                         style={{ display: "flex", justifyContent: "center", flexDirection: "column" }}
                       >
