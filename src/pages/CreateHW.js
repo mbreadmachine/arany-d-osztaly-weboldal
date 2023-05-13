f2b72793efa9c5ba3cad9719e494ab4908e9a86a
Marcus1YouTube <75576363+Marcus1YouTube@users.noreply.github.com>
2023-05-13 11:53:40 +0200

    new function made! done finally...

--- src/pages/CreateHW.js
+++ src/pages/CreateHW.js
@@ -1,8 +1,13 @@
 import React from "react";
 import { NavBar } from "../components/NavBar";
-import { Button, TextField } from "@mui/material";
+import { Button, TextField, Grid, Typography } from "@mui/material";
 import LoginIcon from "@mui/icons-material/Login";
+import SaveIcon from "@mui/icons-material/Save";
 import md5 from "crypto-js/md5";
+import { v4 as uuidv4 } from 'uuid'
+import { HomeWorkCard } from "../components/HomeWorkCard";
+import {supabase} from "../supabase";
+import { redirect } from "react-router-dom";
 
 const CreateHW = () => {
   let [isLoggedIn, setIsLoggedIn] = React.useState(false);
@@ -11,19 +16,18 @@ const CreateHW = () => {
     let [password, setPassword] = React.useState("");
 
     const checkPassword = () => {
-        if (password === ""){
-            alert("Nem adtál meg semmit se!")
-            return;
-        } 
-            
+      if (password === "") {
+        alert("Nem adtál meg semmit se!");
+        return;
+      }
 
-        if (md5(password).toString() === process.env.REACT_APP_CREATE_PASS_HASH) {
-            setIsLoggedIn(true);
-        } else {
-            alert("Hibás jelszó!")
-            setPassword("")
-        }
-    }
+      if (md5(password).toString() === process.env.REACT_APP_CREATE_PASS_HASH) {
+        setIsLoggedIn(true);
+      } else {
+        alert("Hibás jelszó!");
+        setPassword("");
+      }
+    };
 
     return (
       <div
@@ -41,7 +45,11 @@ const CreateHW = () => {
           onChange={(e) => setPassword(e.target.value)}
           type="password"
         />
-        <Button startIcon={<LoginIcon />} style={{ marginTop: "1%" }} onClick={checkPassword}>
+        <Button
+          startIcon={<LoginIcon />}
+          style={{ marginTop: "1%" }}
+          onClick={checkPassword}
+        >
           Bejelentkezés
         </Button>
       </div>
@@ -49,9 +57,84 @@ const CreateHW = () => {
   };
 
   const CreateUI = () => {
+    const [value, setValue] = React.useState("1");
+    const [nowDate, setNowDate] = React.useState(
+      new Date().toISOString().slice(0, 10)
+    );
+
+    const [homework, setHomework] = React.useState([
+      {
+        id: uuidv4(),
+        date: nowDate,
+        homework: "",
+      },
+    ]);
+
+    const handleChange = (event, newValue) => {
+      setValue(newValue);
+    };
+
+    const saveHW = async () => {
+      try {
+        const { error } = await supabase
+        .from("homework")
+        .insert(homework[0]);
+        if (error) throw error.message;
+        alert("Mentve! Az OK gomb megnyomása után visszairányítalak a főoldalra.");
+        window.location.href = "/";
+        return;
+      } catch (err) {
+        alert(err);
+      }
+
+    };
+
     return (
       <div>
-        <p>Logged in!</p>
+        <Grid container sx={{ mt: 2 }} spacing={2}>
+          <Grid item xs={12} md={6}>
+            <Typography variant="h5" textAlign="center">
+              Házi feladat adatok:
+            </Typography>
+            <input
+              type="date"
+              style={{ width: "100%", height: "30px", marginBottom: "1%" }}
+              value={homework[0].date}
+              onChange={(e) =>
+                setHomework([{ ...homework[0], date: e.target.value }])
+              }
+            />
+            <TextField
+              multiline
+              rows="20"
+              fullWidth
+              onChange={(e) =>
+                setHomework([{ ...homework[0], homework: e.target.value }])
+              }
+            />
+            <Button
+              variant="contained"
+              startIcon={<SaveIcon />}
+              style={{ marginTop: "1%" }}
+              fullWidth
+              onClick={saveHW}
+            >
+              Mentés
+            </Button>
+          </Grid>
+          <Grid
+            item
+            xs={12}
+            md={6}
+            style={{
+              display: "flex",
+              alignItems: "center",
+              flexDirection: "column",
+            }}
+          >
+            <HomeWorkCard data={homework} />
+          </Grid>
+        </Grid>
       </div>
     );
   };
